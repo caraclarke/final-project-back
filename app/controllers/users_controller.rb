74b46a9ae83ff6255d933715340d2693a44b424c
caraclarke <cara.clarke8@gmail.com>
2015-09-10 17:28:57 -0400

    Adjusted controller for user profile to show.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,30 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate, only: [:login, :create, :show, :update, :destroy]
+  skip_before_action :authenticate, only: [:login, :create]
   # TODO: user show action
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user, status: :created
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     render json: @user, status: :created
+  #   else
+  #     render json: @user.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   def login
     credentials = user_params
     token = User.login(credentials[:email], credentials[:password])
     user = User.find_by(token: token)
     if token
-      render json: { token: token }
+      render json: { token: token, userId: user.id }
     else
       head :unauthorized
     end
   end
 
   def show
-    @user = User.find(params[:id])
-    render json: @user #, serializer: ProfileSerializer
+    user = User.find(params[:id])
+    render json: user # , serializer: ProfileSerializer
   end
 
   def update
