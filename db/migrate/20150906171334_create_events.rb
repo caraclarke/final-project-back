1eba579c2486eee98b8c096115af263a7137ff25
caraclarke <cara.clarke8@gmail.com>
2015-09-11 00:28:43 -0400

    Changed index to false for event title

--- db/migrate/20150906171334_create_events.rb
+++ db/migrate/20150906171334_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.string :date, index: true
-      t.string :title, index: true
+      t.string :title
       t.text :caption
       t.string :image
       t.references :user, foreign_key: true
